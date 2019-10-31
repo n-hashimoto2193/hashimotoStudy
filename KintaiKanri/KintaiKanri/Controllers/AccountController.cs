4107c8dc62d8bae2129b876542a56aedd5ae77ef
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2019-10-31 11:49:43 +0900

    ユーザー登録　まで

--- KintaiKanri/KintaiKanri/Controllers/AccountController.cs
+++ KintaiKanri/KintaiKanri/Controllers/AccountController.cs
@@ -147,11 +147,11 @@ namespace KintaiKanri.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(Register model)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.SyainName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
